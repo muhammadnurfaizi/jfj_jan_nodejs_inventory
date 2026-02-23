1666c22195c37652aa4b140ae5c28ea286c7f709
muhammadnurfaizi <faiz130201@gmail.com>
2026-02-23 20:14:28 +0700

    Setting aplikasi & route awal

---
+++ app.js
@@ -0,0 +1,17 @@
+const express   = require('express')
+const app       = express()
+const port      = 3000
+const c_beranda = require('./controller/c_beranda')
+
+
+app.use( express.urlencoded({extended:false}) )
+app.set('view engine', 'ejs')
+app.set('views', './view-html')
+
+
+app.get('/', c_beranda.index)
+
+
+app.listen(port, ()=>{
+    console.log(`Aplikasi sudah siap, buka http://localhost:${port}`)
+})
